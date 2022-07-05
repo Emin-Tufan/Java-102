789e89d49aa71b159e7605d9656d4d1ed440f991
Emin-Tufan <93204330+Emin-Tufan@users.noreply.github.com>
2022-07-05 20:05:13 +0300

    Add files via upload

---
+++ FiksturOlusturucu/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Teams teams = new Teams();
+        teams.teamList.add("Galatasaray");
+        teams.teamList.add("BursaSpor");
+        teams.teamList.add("FenerBahce");
+        teams.teamList.add("Besiktas");
+        teams.teamList.add("SivasSpor");
+        teams.teamList.add("TrabzonSpor");
+        teams.ligFixturePrint();
+
+    }
+}
